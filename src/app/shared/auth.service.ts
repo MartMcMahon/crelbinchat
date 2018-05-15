34768cd903a495e8623ff6f6ec14cfe910fba189
mart <matt@paradigmautomation.co>
2018-05-15 15:04:25 -0500

    altered auth service

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,33 +1,63 @@
 import { Injectable } from '@angular/core'
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database'
 import { AngularFireAuth } from 'angularfire2/auth'
-import { Observable } from 'rxjs'
+import { Router } from '@angular/router'
 import * as firebase from 'firebase/app'
-import 'rxjs/add/operator/do'
+
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/switchMap'
+
+
+const userTimeout = 300000 //5 mins
+
+interface User {
+  uid: string
+  email: string
+  phptoUrl?: string
+  displayName?: string
+  crelbinName?: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  user: Observable<User>
   
   usersPath: string = 'users/'
   usersListRef = null
   usersObjRef = null
   connectionRef = null
   userId: string 
+  authState
   
-  constructor(private afAuth: AngularFireAuth, private db: AngularFireDatabase) { 
+  constructor(private afAuth: AngularFireAuth, 
+              private db: AngularFireDatabase, private router: Router) { 
+
+    this.user = this.afAuth.authState
+      .switchMap(user => {
+        if (user) {
+          return this.db.object<User>(`users/${user.uid}`).valueChanges()
+        }
+        else {
+          return Observable.of(null)
+        }
+      })
+
     this.userId = "1"
     this.usersListRef = this.db.list(this.usersPath)
     this.usersObjRef = this.db.object(this.usersPath)
 
-    this.db.object(this.usersPath + this.userId).query.ref.onDisconnect().update({})
+    // this.db.object(this.usersPath + this.userId).query.ref.onDisconnect().update({})
 
-    this.afAuth.authState.do(user => {
-      console.log(user)
-      // this.updateOnConnect()
-      this.updateOnDisconnect()
-    })
+
+    // this.afAuth.authState.pipe(first())
+    // this.afAuth.authState.do(user => {
+    //   console.log(user)
+    //   this.updateOnConnect()
+    //   this.updateOnDisconnect()
+    // })
   }
   
   /**
@@ -36,15 +66,23 @@ export class AuthService {
   signedOn(sender: string) {
     let res = this.usersListRef.push({
       'sender': sender, 
-      'timestamp': new Date().toString()
+      'timestamp': Date.now(),
+      'status': 'online'
     })
     this.userId = res.key
+    // res.onDisconnect()
   }
 
   getUserList(): Observable<{}[]> {
     return this.usersListRef.valueChanges()
       .do(changes => {
-        changes.map(thing => {
+        changes.map(user => {
+          if (user.timestamp < Date.now() - 30000) { //userTimeout) {
+            // bloc.push(user.key)
+            // console.log(user)
+            // console.log(user.key)
+          }
+          
 
           // console.log(thing)
         })
@@ -62,11 +100,28 @@ export class AuthService {
     userRef.update(obj)
   }
 
+  private updateOnConnect() {
+    let con = this.db.object('.info/connected')
+    console.log(con)
+  }
+
   private updateOnDisconnect() {
     console.log('updateondisconnect')
     console.log(firebase.database().ref())
     firebase.database().ref().child('users/' + this.userId)
       .onDisconnect()
       .update({'status': 'offline'})
+  }
+
+  get authenticated(): boolean {
+    return this.authState !== null
+  }
+
+  get currentUser(): any {
+    return this.authenticated ? this.authState.auth : null
+  }
+
+  private signIn(provider: number): firebase.Promise<FirebaseAuthState> {
+    
   }
 }
